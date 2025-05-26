bddecb627c8dcb30f04c9496174059d337b47890
Ben-Worrall <benworrallpc@gmail.com>
2025-05-26 21:27:06 +0200

    app works with basic outline of homepage

--- public/main.js
+++ public/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
+const http = require('http');
 
 let mainWindow;
 
@@ -13,8 +14,34 @@ function createWindow() {
       contextIsolation: true,
     },
   });
+     mainWindow.setMenu(null); // Hide the menu bar
+  const reactDevServerURL = 'http://localhost:3000';
 
-  mainWindow.loadURL('http://localhost:3000');
+  // Function to check if React server is running
+  const checkReactReady = (url, callback) => {
+    const req = http.get(url, () => {
+      callback(true);
+    });
+
+    req.on('error', () => {
+      callback(false);
+    });
+
+    req.end();
+  };
+
+  // Retry until the React dev server is ready
+  const tryLoadURL = () => {
+    checkReactReady(reactDevServerURL, (isReady) => {
+      if (isReady) {
+        mainWindow.loadURL(reactDevServerURL);
+      } else {
+        setTimeout(tryLoadURL, 300); // Retry in 300ms
+      }
+    });
+  };
+
+  tryLoadURL();
 }
 
 app.whenReady().then(createWindow);
--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useEffect } from 'react';
 
 function App() {
 
   useEffect(() => {
+    console.log('useffect start')
     if (window.electronAPI?.notifyReactReady) {
+      console.log('useffect notifyReactReady')
       window.electronAPI.notifyReactReady();
     }
   }, []);
 
   return (
     <div className="App">
+        {/*  */}
 
-      
-      
+
+      {/* book mark (top border)  */}
+      <div id='top-bar'>bookmarks</div>
+
+      {/*  url bar */}
+      <div id='url-bar'>url</div>
+
+      {/*  options left */}
+      <div id='options-left'>
+        <button className='ScreenOptionButton'>Option 1</button>
+        <button className='ScreenOptionButton'>Option 2</button>
+      </div>
+
+      {/*  options right */}
+      <div id='options-right'>
+        <button className='ScreenOptionButton'>Option 3</button>
+        <button className='ScreenOptionButton'>Option 4</button>
+      </div>
+
+      {/*  search bar */}
+      <div id='search-bar'>
+        <input id='search-bar-input' type='text' placeholder='Search...' />
+        <button id='search-bar-button' className='SearchButton'>S</button>
+      </div>
+
     </div>
   );
 }
